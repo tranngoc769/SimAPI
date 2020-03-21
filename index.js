20d99371fca228a3ef51478dd2395c12842c6e49
Trần Ngọc Quang <56824077+tranngoc769@users.noreply.github.com>
2020-03-22 00:10:56 +0700

    finish view

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const app = express();
+var path = require('path');
+
+
+var bodyParser = require("body-parser");
+
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname, '/public')));
+/* @@END VIEWS ENGINE */
+
+/* BODY_PARSER */
+app.use(bodyParser.json({limit: '50mb', extended: true}))
+app.use(bodyParser.urlencoded({limit: '60mb', extended: true}))
+/* END @@BODY_PARSER */
+
+
 require('dotenv').config();
 const port = process.env.PORT  || 5000;
 
+
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname, '/public')));
 
 
 const route = require('./route/route');
--- route/route.js
+++ route/route.js
@@ -4,98 +4,84 @@ const key = '7oqzwpewVVrx+xZcY/7Opzmg68VBxqp5++Pj/NoP';
 const deletefirstLines = require('firstline_delete');
 const firstline = require('firstline');
 const fs = require('fs');
-function apiRequest(key,reqText,lang,res)
-  {
-    try
-    {
-      firstline('./key', { lineEnding: '\n'})
-      .then((key) => 
-      {
-      if (key == 'Poor')
-      {
-      var jsonString  = `{\"messages\": [{\"text\":\"Key het roi, huhu\"}]}`;
-      var jsonObj = JSON.parse(jsonString);
-      res.send(jsonObj);
-        fs.writeFile('./key', 'Poor', function (err) {
-          if (err) throw err;
-          console.log('Saved!');
-        });
-        return;
-      }
-      var options = {
-        method: 'POST',
-        url: 'https://wsapi.simsimi.com/190410/talk/',
-        headers: 
-        { 'x-api-key': key,
-          'Content-Type': 'application/json' },
-        body: { utext: reqText, lang: `${lang}`,atext_bad_prob_max : 0.3 },
-        json: true 
-      };  
-      const r = request(options, function (error, response, body) {
-        if (error) throw new Error(error);
-        if (body.message == 'Limit Exceeded Exception' || body.message == 'Forbidden')
-        {
-          deletefirstLines('./key');
-          apiRequest(key,reqText,lang,res);
-        }
-        else
-        {
-          var jsonString  = `{\"messages\": [{\"text\":\"${body.atext}\"}]}`;
+function apiRequest(key, reqText, lang, res) {
+  try {
+    firstline('./key', { lineEnding: '\n' })
+      .then((key) => {
+        if (key == 'Poor') {
+          var jsonString = `{\"messages\": [{\"text\":\"Key het roi, huhu\"}]}`;
           var jsonObj = JSON.parse(jsonString);
           res.send(jsonObj);
+          fs.writeFile('./key', 'Poor', function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+          });
+          return;
         }
-      });
-      
-      console.log(r.headers);
+        var options = {
+          method: 'POST',
+          url: 'https://wsapi.simsimi.com/190410/talk/',
+          headers:
+          {
+            'x-api-key': key,
+            'Content-Type': 'application/json'
+          },
+          body: { utext: reqText, lang: `${lang}`, atext_bad_prob_max: 0.3 },
+          json: true
+        };
+        const r = request(options, function (error, response, body) {
+          if (error) throw new Error(error);
+          if (body.message == 'Limit Exceeded Exception' || body.message == 'Forbidden') {
+            deletefirstLines('./key');
+            apiRequest(key, reqText, lang, res);
+          }
+          else {
+            var jsonString = `{\"messages\": [{\"text\":\"${body.atext}\"}]}`;
+            var jsonObj = JSON.parse(jsonString);
+            res.send(jsonObj);
+          }
+        });
+
+        console.log(r.headers);
       }
       );
-      
-    }
-    catch(err)
-    {
-      var jsonString  = `{\"messages\": [{\"text\":\"Loi roi, huhu\"}]}`;
-      var jsonObj = JSON.parse(jsonString);
-          res.send(jsonObj);
-    }
-  } 
 
-module.exports = 
+  }
+  catch (err) {
+    var jsonString = `{\"messages\": [{\"text\":\"Loi roi, huhu\"}]}`;
+    var jsonObj = JSON.parse(jsonString);
+    res.send(jsonObj);
+  }
+}
+
+module.exports =
 {
-    router(app) 
-{  
-  app.route('/api/text=:text&lan=:lan')
+  router(app) {
+    app.route('/api/text=:text&lan=:lan')
       .get(function (req, res) {
-          var reqText = req.params['text']; 
-          var lang = req.params['lan']; 
-          apiRequest(key,reqText,lang,res)
-          });
-  
-          app.route('/')
-          .get(function (req, res) {
-              res.send('Server started')
-              });
-              
-      app.route('')
+        var reqText = req.params['text'];
+        var lang = req.params['lan'];
+        apiRequest(key, reqText, lang, res)
+      });
+
+    app.route('/')
       .get(function (req, res) {
-          res.send('Server started')
-          });
-      
-              
-      app.route('/api/text=:text')
+        res.render('index');
+      });
+
+    app.route('')
       .get(function (req, res) {
-          var reqText = req.params['text']; 
-          var lang = 'vi'; 
-          apiRequest(key,reqText,lang,res)
-          });
-  
+        res.send('Server started')
+      });
 
 
+    app.route('/api/text=:text')
+      .get(function (req, res) {
+        var reqText = req.params['text'];
+        var lang = 'vi';
+        apiRequest(key, reqText, lang, res)
+      });
 
 
-
-
-
-
-
-} 
+  }
 }
